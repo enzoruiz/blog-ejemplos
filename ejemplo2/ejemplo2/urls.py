5155ddf9d290d10ecc67ae21f81be93d21fd9f41
Enzo <enzo.rp.90@gmail.com>
2017-10-04 16:53:14 -0500

    code of ejemplo2

--- ejemplo2/ejemplo2/urls.py
+++ ejemplo2/ejemplo2/urls.py
@@ -1,4 +1,4 @@
-"""ejemplo1 URL Configuration
+"""ejemplo2 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -13,15 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
-from django.contrib.auth.views import logout
-from inicio.views import LoginView, HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('', include('social_django.urls', namespace='social')),
-    url(r'^login$', LoginView.as_view(), name='login'),
-    url(r'^home$', HomeView.as_view(), name='home'),
-    url(r'^logout$', logout, name='logout'),
 ]
--- ejemplo2/ejemplo2/wsgi.py
+++ ejemplo2/ejemplo2/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ejemplo1 project.
+WSGI config for ejemplo2 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ejemplo1.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ejemplo2.settings")
 
 application = get_wsgi_application()
